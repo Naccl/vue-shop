1a5ee83c23878fbd76df5fd3eefd8ce3e596d29f
Naccl <admin@naccl.top>
2020-07-12 15:08:07 +0800

    添加babel插件优化console

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
+//项目发布阶段使用的babel插件
+const prodPlugins = []
+if (process.env.NODE_ENV === 'production') {
+	prodPlugins.push('transform-remove-console')
+}
+
 module.exports = {
-  "presets": [
-    "@vue/cli-plugin-babel/preset"
-  ]
+	"presets": [
+		"@vue/cli-plugin-babel/preset"
+	],
+	'plugins': [
+		...prodPlugins
+	]
 }
